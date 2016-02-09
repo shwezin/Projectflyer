34508d6f756860b12b0c0e8f0877c6c6da7cfaf0
shwezin <zadeveloper02@gmail.com>
2016-02-09 13:02:09 +0630

    pull

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+
+<div class="jumbotron">
+	<h1>Project Flyer</h1>
+
+	<p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+
+	<button class="btn btn-primary">Create a Flyer</button>
+</div>
+
+@stop
